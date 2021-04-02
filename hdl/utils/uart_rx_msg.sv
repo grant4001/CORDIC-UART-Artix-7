c2d9f0b412abec30ac0ad56a81a78c71ff2c3891
GrantYu <gy.4002@gmail.com>
2021-04-02 13:08:46 -0700

    GPL License added.

--- hdl/utils/uart_rx_msg.sv
+++ hdl/utils/uart_rx_msg.sv
@@ -6,6 +6,21 @@
 //              codes and sequences are implemented, and a Galois LFSR is wrapped within this module 
 //              to compute the CRC-8.
 //
+// Copyright (C) 2021, Grant Yu
+//
+// This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//    You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 
 `default_nettype none
 
